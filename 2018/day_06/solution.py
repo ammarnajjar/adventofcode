61d87f1f7229aeb93a8176f3d56c1821a67cb54b
Ammar Najjar <najjarammar@protonmail.com>
2019-11-04 22:31:26 +0100

    feat: parse str to points

--- 2018/day_06/solution.py
+++ 2018/day_06/solution.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass
 class Point:
     x: int
     y: int
+
+
+def str_to_points(input_data: str) -> List[Point]:
+    r = []
+    for line in input_data.strip().split('\n'):
+        # each line has coordinates for exactly one point
+        x, y = [int(a.strip()) for a in line.split(',')]
+        r.append(Point(x, y))
+    return r
 
 
 def manhatten_distance(p1: Point, p2: Point) -> int:
--- 2018/day_06/test_solution.py
+++ 2018/day_06/test_solution.py
@@ -3,42 +3,59 @@ import pytest
 
 from .solution import manhatten_distance
 from .solution import Point
+from .solution import str_to_points
 
-input_points = [
+input_manhatten_str = [
     (
-        Point(1, 2),
-        Point(1, 2),
+        '1, 2\n1, 2\n',
         0,
     ),
     (
-        Point(2, 2),
-        Point(1, 2),
+        '2, 2\n1, 2\n',
         1,
     ),
     (
-        Point(1, 2),
-        Point(2, 2),
+        '1, 2\n2, 2\n',
         1,
     ),
     (
-        Point(1, 2),
-        Point(2, 1),
+        '1, 2\n2, 1\n',
         2,
     ),
     (
-        Point(1, 2),
-        Point(2, 1),
+        '2, 1\n1, 2\n',
         2,
     ),
 ]
 
+points_str = [
+    (
+        '1, 2\n1, 2\n1, 2\n1, 2\n',
+        [Point(1, 2)] * 4,
+    ),
+    (
+        '2, 2\n1, 2\n2, 2\n1, 2\n',
+        [Point(2, 2), Point(1, 2)] * 2,
+    ),
+]
 
-@pytest.fixture(params=input_points)
+
+@pytest.fixture(params=points_str)
+def input_points_str(request):
+    return request.param
+
+
+@pytest.fixture(params=input_manhatten_str)
 def input_points_part1(request):
     return request.param
 
 
 class TestDay06Part01:
+    def test_str_to_points(self, input_points_str):
+        points_string, expected = input_points_str
+        assert str_to_points(points_string) == expected
+
     def test_manhatten_distance(self, input_points_part1):
-        p1, p2, distance = input_points_part1
+        points_string, distance = input_points_part1
+        p1, p2 = str_to_points(points_string)
         assert manhatten_distance(p1, p2) == distance
