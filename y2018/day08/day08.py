8c69960c24833f8c6c2783d0739dc1eaf8e1aef1
Ammar Najjar <najjarammar@protonmail.com>
2019-11-11 22:55:50 +0100

    feat: day 8 part 2

--- y2018/day08/day08.py
+++ y2018/day08/day08.py
@@ -1,28 +1,47 @@
-"""Part 1"""
 import os
 from typing import List
 from typing import Tuple
 
 
-def parse(input_data: List[int]) -> Tuple[int, List[int]]:
+def parse(input_data: List[int]) -> Tuple[int, int, List[int]]:
     n_child, n_meta = input_data[:2]
     input_data = input_data[2:]
     totals = 0
+    data_values = []
     for _ in range(n_child):
-        total, input_data = parse(input_data)
+        total, data_value, input_data = parse(input_data)
         totals += total
+        data_values.append(data_value)
     totals += sum(input_data[:n_meta])
-    return totals, input_data[n_meta:]
+    if n_child == 0:
+        data_value = sum(input_data[:n_meta])
+    else:
+        data_value = sum(
+            # list index starts at 0, meta index starts at 1
+            data_values[i - 1]
+            for i in input_data[:n_meta]
+            # ignore if no children referenced
+            if 0 < i <= len(data_values)
+        )
+    return totals, data_value, input_data[n_meta:]
 
 
 def part1(input_text: str) -> int:
     x = [int(c) for c in input_text.strip().split()]
-    return parse(x)[0]
+    p = parse(x)
+    return p[0]
+
+
+def part2(input_text: str) -> int:
+    x = [int(c) for c in input_text.strip().split()]
+    p = parse(x)
+    return p[1]
 
 
 if __name__ == '__main__':  # pragma no cover
     current_path = os.path.dirname(os.path.realpath(__file__))
     with open(f'{current_path}/input08', 'r') as input_file:
         input_text = input_file.read().strip()
-    # solution is between 55110 and 35713 -> 40977
+    # part1 solution is between 55110 and 35713 -> 40977
     print(part1(input_text))
+    print(part2(input_text))
--- y2018/day08/day08_test.py
+++ y2018/day08/day08_test.py
@@ -1,8 +1,9 @@
 import pytest
 from day08 import part1
+from day08 import part2
 
 
-input_part1 = [
+input_part1 = (
     (
         '2 2 '
         '1 1 '
@@ -48,7 +49,7 @@ input_part1 = [
         '1 1 1 1 0 1 99 99 99',
         99 * 3,
     ),
-]
+)
 
 
 @pytest.fixture(params=input_part1)
@@ -60,3 +61,48 @@ class TestDay8:
     def test_part1(self, sample_part1):
         input_text, expected = sample_part1
         assert part1(input_text) == expected
+
+
+input_sample = (
+    (
+        '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2',
+        66,
+    ),
+    (
+        '2 3 0 3 10 11 12 1 1 0 1 99 1 1 1 2',
+        66 + 99,
+    ),
+    (
+        '2 2 '
+        '1 1 '
+        '0 2 1 1 '
+        '1 '
+        '0 1 1 '
+        '2 2',
+        2,
+    ),
+    (
+        '3 3 '
+        '2 2 '  # 2
+        '1 1 '
+        '0 2 1 1 '
+        '1 '
+        '0 1 1 '
+        '2 2 '
+        '2 2 '  # 4
+        '0 1 1 '
+        '0 2 1 1 '
+        '2 2 '
+        '0 3 1 1 1 '  # 3
+        '1 2 3',
+        2 + 4 + 3,
+    ),
+)
+
+
+@pytest.mark.parametrize(
+    'par', input_sample,
+)
+def test_part2(par):
+    s, r = par
+    assert part2(s) == r
