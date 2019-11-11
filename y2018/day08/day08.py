04342b0f55fc6a6d2243ff160a9123a09f90d31a
Ammar Najjar <najjarammar@protonmail.com>
2019-11-11 17:56:43 +0100

    feat: day 8 part 1

--- y2018/day08/day08.py
+++ y2018/day08/day08.py
@@ -1,28 +1,28 @@
 """Part 1"""
 import os
+from typing import List
+from typing import Tuple
+
+
+def parse(input_data: List[int]) -> Tuple[int, List[int]]:
+    n_child, n_meta = input_data[:2]
+    input_data = input_data[2:]
+    totals = 0
+    for _ in range(n_child):
+        total, input_data = parse(input_data)
+        totals += total
+    totals += sum(input_data[:n_meta])
+    return totals, input_data[n_meta:]
 
 
 def part1(input_text: str) -> int:
-    nodes = []
-    rest = [int(c) for c in input_text.strip().split()]
-    while rest:
-        n_child = rest[0]
-        n_meta = rest[1]
-        if n_child > 0 and n_meta > 0:
-            meta = rest[-n_meta:]
-            rest = rest[2:-n_meta]
-        else:
-            meta = rest[2:2 + n_meta]
-            rest = rest[2 + n_meta:]
-        nodes.append((n_child, n_meta, meta))
-    return sum(
-        sum(n[2]) for n in nodes
-    )
+    x = [int(c) for c in input_text.strip().split()]
+    return parse(x)[0]
 
 
 if __name__ == '__main__':  # pragma no cover
     current_path = os.path.dirname(os.path.realpath(__file__))
     with open(f'{current_path}/input08', 'r') as input_file:
         input_text = input_file.read().strip()
-    # solution between 55110 and 35713
+    # solution is between 55110 and 35713 -> 40977
     print(part1(input_text))
